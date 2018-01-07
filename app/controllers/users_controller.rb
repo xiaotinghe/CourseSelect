7f0d77850fd46cdc133b9361414f624fdabf6f61
qdhlbt <qdhlbt@163.com>
2018-01-08 01:38:05 +0800

    由 Coding WebIDE 更新，https://ide.coding.net

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,16 +12,17 @@ class UsersController < ApplicationController
         require 'net/http'
         require 'net/https'
         require 'json'
-
+        cache = ActiveSupport::Cache::MemoryStore.new
+        cache.write(params[:uuid] + '_img', params[:base64])
         base64 = params[:base64].gsub("data:image/jpeg;base64,", "")
-        uri = URI('https://aip.baidubce.com/rest/2.0/ocr/v1/general')
+        uri = URI('https://aip.baidubce.com/rest/2.0/ocr/v1/general?access_token=24.0472f5b2823bda4b9a3fed3fa6d4b6d0.2592000.1515502474.282335-10508030')
         req = Net::HTTP::Post.new(uri)
         req.set_form_data('image' => base64)
         res = Net::HTTP.start(uri.hostname, uri.port,:use_ssl => uri.scheme == 'https',:open_timeout => 1) do |http|
             http.request(req)
         end
         case res
-
+        
         when Net::HTTPSuccess, Net::HTTPRedirection
             render :json => {:base64 => "Response #{res.code} #{res.message}: #{res.body}"}.to_json  
         else
