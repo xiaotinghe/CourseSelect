fb6d364ae25465d2aa68f649642f2f5e7c9e00be
qdhlbt <qdhlbt@163.com>
2018-01-08 05:07:24 +0800

    由 Coding WebIDE 更新，https://ide.coding.net

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -273,7 +273,11 @@ class CoursesController < ApplicationController
       redirect_to root_url, flash: {danger: '请登陆'}
     end
   end
-
+  def admin_logged_in
+    unless admin_logged_in?
+      redirect_to root_url, flash: {danger: '请登陆'}
+    end
+  end
   # Confirms a  logged-in user.
   def logged_in
     unless logged_in?
