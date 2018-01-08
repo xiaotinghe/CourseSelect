485a3d7a8b32999c3cd0bb24e20a6f2e0b323c8f
qdhlbt <qdhlbt@163.com>
2018-01-08 10:47:34 +0800

    由 Coding WebIDE 更新，https://ide.coding.net

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     end
     def mobile
         @uuid = params[:uid]
-        render "mobile"
+        render layout:  "mobile"
     end
     def checked
         if Rails.cache.read(params[:uid])
